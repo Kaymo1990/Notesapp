46d84095bcadb10c7ca62a26cbac39796db63c77
Kay <khurrammohammed1990@gmail.com>
2019-10-22 17:53:01 +0100

    updating app with list and read methods

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
+const yargs = require('yargs');
 const command = process.argv[2];
 
-console.log(process.argv);
+yargs.command({
+command: 'add',
+describe: 'Add a new note',
+handler: function() {
+    console.log('Adding a new note');
+    }
+})
 
-if (command === 'add') {
-    console.log("Adding note");
-}
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing the note');
+    }
+})
 
-else if (command === 'remove') {
-    console.log("Removing note");
-}
+yargs.command({
+    command: 'list',
+    describe: 'Lists notes',
+    handler: function() {
+        console.log('Listing notes');
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Reads notes',
+    handler: function() {
+        console.log('Reading notes');
+    }
+})
+
+console.log(yargs.argv);
